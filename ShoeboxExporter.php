5e479cb6cb10898e0362a9d7f96efaa707906ddc
Chris Hayen <chayen@gmail.com>
2014-11-13 15:57:35 -0500

    Exporter 0.1

---
+++ ShoeboxExporter.php
@@ -0,0 +1,87 @@
+<?php
+
+if(count($argv) < 3)
+{
+  echo "Usage: php ShoeboxExporter.php <export file> <directory to save to>\n";
+  return;
+}
+else
+{
+  $exportData = $argv[1];
+  $outputDirectory = $argv[2];
+
+  $exporter = new ShoeboxExporter();
+  $exporter->export($exportData, $outputDirectory);
+
+}
+
+class ShoeboxExporter
+{
+
+  public function export($exportFile, $outputDirectory)
+  {
+    $directoryPrefix = "ShoeboxExport";
+    $exportData = file($exportFile);
+    $count = count($exportData);
+
+    for($i=0; $i<$count; $i++)
+    {
+      $lineItem = $exportData[$i];
+
+      $directory  = $outputDirectory . '/' . $directoryPrefix . '/';
+      $directory .= $this->directoryFor($lineItem) . '/';
+
+      $fileName = $this->fileNameFor($lineItem);
+      $fileSize = $this->fileSizeFor($lineItem);
+      $url = $this->urlFor($lineItem);
+
+      $this->download($fileName, $fileSize, $directory, $url, $i, $count);
+    }
+  }
+
+  private function download($fileName, $fileSize, $directory, $url, $index, $total)
+  {
+    $fullPath = $directory . $fileName;
+    echo '(' . ($index+1) . ' of ' . $total . ') ' . $fullPath . ' ';
+
+    if(!file_exists($directory)) mkdir($directory, 0755, true);
+    if(file_exists($fullPath))
+    {
+      echo "Skipping\n";
+    }
+    else
+    {
+      echo "Downloading\n";
+      $data = file_get_contents($url);
+      file_put_contents($fullPath, $data);
+    }
+  }
+
+  private function directoryFor($lineItem)
+  {
+    $lineParts = explode(',', $lineItem);
+    $fileParts = explode('/', $lineParts[0]);
+    return $fileParts[0];
+  }
+
+  private function fileNameFor($lineItem)
+  {
+    $lineParts = explode(',', $lineItem);
+    $fileParts = explode('/', $lineParts[0]);
+    return $fileParts[1];
+  }
+
+  private function urlFor($lineItem)
+  {
+    $lineParts = explode(',', $lineItem);
+    return $lineParts[2];
+  }
+
+  private function fileSizeFor($lineItem)
+  {
+    $lineParts = explode(',', $lineItem);
+    return $lineParts[1];
+  }
+}
+
+?>
